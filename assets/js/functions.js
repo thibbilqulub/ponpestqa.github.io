57449650c8594d8c4d388eaa1966b16a4d5ef93b
ponpespmk <ponpespmk@gmail.com>
2021-12-04 11:34:13 +0700

    Office 12/04/2021 | Randown

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -361,45 +361,36 @@
     });
 
 
-    //Countdown js initialization
-    document.addEventListener('readystatechange', event => {
-        if (event.target.readyState === "complete") {
-            var clockdiv = document.getElementsByClassName("event-count");
-            var countDownDate = new Array();
-            for (var i = 0; i < clockdiv.length; i++) {
-                countDownDate[i] = new Array();
-                countDownDate[i]['el'] = clockdiv[i];
-                countDownDate[i]['time'] = new Date(clockdiv[i].getAttribute('data-date')).getTime();
-                countDownDate[i]['days'] = 0;
-                countDownDate[i]['hours'] = 0;
-                countDownDate[i]['seconds'] = 0;
-                countDownDate[i]['minutes'] = 0;
-            }
+    // Set CountDown
+    var countDownDate = new Date("Feb 1, 2022 23:00:00").getTime();
 
-            var countdownfunction = setInterval(function() {
-                for (var i = 0; i < countDownDate.length; i++) {
-                    var now = new Date().getTime();
-                    var distance = countDownDate[i]['time'] - now;
-                    countDownDate[i]['days'] = Math.floor(distance / (1000 * 60 * 60 * 24));
-                    countDownDate[i]['hours'] = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-                    countDownDate[i]['minutes'] = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-                    countDownDate[i]['seconds'] = Math.floor((distance % (1000 * 60)) / 1000);
+    // Update the count down every 1 second
+    var x = setInterval(function() {
 
-                    if (distance < 0) {
-                        countDownDate[i]['el'].querySelector('.days').innerHTML = 0;
-                        countDownDate[i]['el'].querySelector('.hours').innerHTML = 0;
-                        countDownDate[i]['el'].querySelector('.minutes').innerHTML = 0;
-                        countDownDate[i]['el'].querySelector('.seconds').innerHTML = 0;
-                    } else {
-                        countDownDate[i]['el'].querySelector('.days').innerHTML = countDownDate[i]['days'];
-                        countDownDate[i]['el'].querySelector('.hours').innerHTML = countDownDate[i]['hours'];
-                        countDownDate[i]['el'].querySelector('.minutes').innerHTML = countDownDate[i]['minutes'];
-                        countDownDate[i]['el'].querySelector('.seconds').innerHTML = countDownDate[i]['seconds'];
-                    }
-                }
-            }, 1000);
+        // Dapatkan tanggal dan waktu hari ini
+        var now = new Date().getTime();
+
+        // Temukan jarak antara sekarang dan tanggal hitung mundur
+        var distance = countDownDate - now;
+
+        // Perhitungan waktu untuk hari, jam, menit dan detik
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Tampilkan hasilnya dalam elemen dengan id yang ditentukan
+        document.getElementById("hari").innerHTML = days;
+        document.getElementById("jam").innerHTML = hours;
+        document.getElementById("menit").innerHTML = minutes;
+        document.getElementById("detik").innerHTML = seconds;
+
+        // Jika hitungan mundur selesai, tulis Pesan teks
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("demo").innerHTML = "EXPIRED";
         }
-    });
+    }, 1000);
 }(jQuery));
 
 // <script>
