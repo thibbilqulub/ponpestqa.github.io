c22a92b2c34ddc12d3598eb20417a3b7b659b879
ponpespmk <ponpespmk@gmail.com>
2021-12-07 16:11:47 +0700

    Office 12/07/2021 | Home Page

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -359,38 +359,6 @@
         }, 2000);
 
     });
-
-
-    // Set CountDown
-    var countDownDate = new Date("Feb 1, 2022 23:00:00").getTime();
-
-    // Update the count down every 1 second
-    var x = setInterval(function() {
-
-        // Dapatkan tanggal dan waktu hari ini
-        var now = new Date().getTime();
-
-        // Temukan jarak antara sekarang dan tanggal hitung mundur
-        var distance = countDownDate - now;
-
-        // Perhitungan waktu untuk hari, jam, menit dan detik
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-        // Tampilkan hasilnya dalam elemen dengan id yang ditentukan
-        document.getElementById("hari").innerHTML = days;
-        document.getElementById("jam").innerHTML = hours;
-        document.getElementById("menit").innerHTML = minutes;
-        document.getElementById("detik").innerHTML = seconds;
-
-        // Jika hitungan mundur selesai, tulis Pesan teks
-        if (distance < 0) {
-            clearInterval(x);
-            document.getElementById("demo").innerHTML = "EXPIRED";
-        }
-    }, 1000);
 }(jQuery));
 
 // <script>
